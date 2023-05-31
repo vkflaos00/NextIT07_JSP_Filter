19bba9f6f4121e6eb5723ae20ed3cc271fa88f60
pc21 <pc21@pc21>
2023-05-31 12:50:36 +0900

    2023 05 31

--- src/kr/or/nextit/login/web/Login.java
+++ src/kr/or/nextit/login/web/Login.java
@@ -10,24 +10,10 @@ public class Login implements NextITProcess{
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
-		
+
 		String msg = request.getParameter("msg");
 		System.out.println("msg : "+ msg);
-		
-		/*if(msg !=null && msg.equals("null")){
-			out.print("<script>");
-			out.print("alert('ID 또는 PASSWORD를 입력해주세요')");
-			out.print("</script>");
-		}else if(msg !=null && msg.equals("fail")){
-			out.print("<script>");
-			out.print("alert('로그인 실패하였습니다. ID 또는 PASSWORD를 확인해주세요.')");
-			out.print("</script>");
-		}else if(msg !=null && msg.equals("none")){
-			out.print("<script>");
-			out.print("alert('로그인 하셔야 이용 가능합니다.')");
-			out.print("</script>");
-		}*/
+
 		request.setAttribute("msg", msg);
 
 		Cookie[] cookies =  request.getCookies();
